b0b0fe1d70c9eb92b9a4b7bf874640c267f48684
𝐋ɪɴᴜх 𝐒ᴇʀ 👽📈 <callmebro2244@gmail.com>
2026-05-16 20:35:30 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐋ɪɴᴜх 𝐒ᴇʀ 🧃🕊️',
+  author: '‎',
+  botName: "𝐋ɪɴᴜх 𝐒ᴇʀ 🧃🕊️",
+  botOwner: '𝐋ɪɴᴜх 𝐒ᴇʀ 🧃🕊️', // Your name
+  ownerNumber: '919562530542', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/linux-ser/Linux_ser/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
