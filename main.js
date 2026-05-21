5b5f33a5b3fbdfbf5e1e8450e239cd817447586c
𝐋ɪɴᴜх 𝐒ᴇʀ 👽📈 <callmebro2244@gmail.com>
2026-05-21 11:23:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ const { autoreadCommand, isAutoreadEnabled, handleAutoread } = require('./comman
 // Command imports
 const tagAllCommand = require('./commands/tagall');
 const pinCommand = require('./commands/pin');
+const unpinCommand = require('./commands/unpin');
 const helpCommand = require('./commands/help');
 const banCommand = require('./commands/ban');
 const { promoteCommand } = require('./commands/promote');
@@ -489,6 +490,10 @@ async function handleMessages(sock, messageUpdate, printLog) {
                 break;
             case userMessage === '.pin':
                 await pinCommand(sock, chatId, message);
+                commandExecuted = true;
+                break;
+            case userMessage === '.unpin':
+                await unpinCommand(sock, chatId, message);
                 commandExecuted = true;
                 break;
             case userMessage === '.tagall':
